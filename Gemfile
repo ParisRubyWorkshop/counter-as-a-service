c70a51c562a67cab68aee55add8ab71150e0fee5
Victor Mours <victor.mours@gmail.com>
2017-03-15 10:57:50 +0100

    Basic version

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'sinatra', '2.0.0.beta2'
+gem 'redis'
+
---
+++ counter.rb
@@ -0,0 +1,30 @@
+require 'bundler/setup'
+require 'sinatra/base'
+
+class Counter < Sinatra::Base
+
+	get '/:counter_name' do
+    {
+      name: params["counter_name"],
+      value: redis.get(params["counter_name"]) || 0
+    }.to_json
+	end
+
+  post '/:counter_name/increment' do
+    {
+      value: redis.incr(params["counter_name"])
+    }
+  end
+
+  post '/:counter_name/decrement' do
+    {
+      value: redis.decr(params["counter_name"])
+    }
+  end
+
+  def redis
+    Redis.new(url: ENV['REDIS_URL'])
+  end
+
+end
+
